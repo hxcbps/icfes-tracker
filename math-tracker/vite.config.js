958d977166936f57f8f8d5d1d73f05a8c1a6c5cd
hxcbps <hxcbps@gmail.com>
2025-12-22 18:44:11 -0500

    Deploy Pages under /math-tracker

--- math-tracker/vite.config.js
+++ math-tracker/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/icfes-tracker/' : '/',
+  base: command === 'build' ? '/icfes-tracker/math-tracker/' : '/',
+  build: {
+    outDir: 'dist/math-tracker',
+  },
   plugins: [react()],
 }))
